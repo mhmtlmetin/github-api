5e5f595dd2553241bf3d7d101331f80bdb7a1980
Mehmet Ali <mhmtlmetin@gmail.com>
2020-04-15 00:24:11 +0300

    detail ozellestirildi

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
-
+import Profile from './Profile';
+import styled from 'styled-components/native';
 //redux
 import {useSelector} from 'react-redux';
 function Detail({navigation}) {
@@ -9,12 +10,28 @@ function Detail({navigation}) {
   console.log(userinfo);
   return (
     <View>
-      {userinfo !== '' &&
-        Object.entries(userinfo).map((key, id) => {
-          return <Text>{key}</Text>;
-        })}
+      <Box>
+        <Profile props={userinfo.avatar_url} />
+        <Info> {userinfo.name}</Info>
+        <Info>takipçi: {userinfo.followers}</Info>
+        <Info>takip: {userinfo.following}</Info>
+      </Box>
+      <Text>login: {userinfo.login}</Text>
+      <Text>bio: {userinfo.bio}</Text>
+      <Text>şirket: {userinfo.company}</Text>
+      <Text>oluşturulma tarihi: {userinfo.created_at}</Text>
     </View>
   );
 }
-
+const Box = styled.View`
+  display: flex;
+  align-items: center;
+  padding: 50px;
+  background: #0fcad8;
+`;
+const Info = styled.Text`
+  color: white;
+  margin: 20px;
+  font-size: 30px;
+`;
 export default Detail;
