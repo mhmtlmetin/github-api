7a0196ad5d6c0d835ca637d8d3946bc29825fd27
Mehmet Ali <mhmtlmetin@gmail.com>
2020-04-20 00:07:34 +0300

    takipci listelendi

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useMemo} from 'react';
 import {
   Alert,
   Modal,
@@ -6,22 +6,42 @@ import {
   Text,
   TouchableHighlight,
   View,
+  ScrollView,
 } from 'react-native';
 //redux
 import {useSelector, useDispatch} from 'react-redux';
-import {loadFollowers} from '../redux/actions';
-const FollowModal = ({props}) => {
+import {loadFollowers, loadUserInfo} from '../redux/actions';
+//components
+import Profile from './Profile';
+//style
+import styled from 'styled-components/native';
+import {black} from 'color-name';
+import Detail from './Detail';
+
+function FollowModal({props}) {
   const [modalVisible, setModalVisible] = useState(false);
+
   const dispatch = useDispatch();
-  const followers = url => dispatch(loadFollowers(url));
-  followers(props);
+  const selectedFollowers = useSelector(state => state.followers);
+  const userinfo = useSelector(state => state.userinfo);
+
+  const followersInfo = text => dispatch(loadUserInfo(text));
+
+  //Open Modal
+  const handleClick = () => {
+    setModalVisible(true);
+  };
+
+  //detay
+  // const userInfo = info => dispatch(userInfo(info));
+
+  const DetailPage = login => {
+    followersInfo({user: login});
+    setModalVisible(false);
+  };
   return (
     <View style={styles.centeredView}>
-      <TouchableHighlight
-        style={styles.openButton}
-        onPress={() => {
-          setModalVisible(true);
-        }}>
+      <TouchableHighlight style={styles.openButton} onPress={handleClick}>
         <Text style={styles.textStyle}>Show Modal</Text>
       </TouchableHighlight>
       <Modal
@@ -33,8 +53,23 @@ const FollowModal = ({props}) => {
         }}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>{props}</Text>
+            {/* <Text style={styles.modalText}>{props}</Text> */}
 
+            <ScrollView>
+              {selectedFollowers != '' &&
+                selectedFollowers.map(item => {
+                  const {login, avatar_url} = item;
+                  return (
+                    <Box>
+                      <Profile props={avatar_url} />
+                      <Info>{login}</Info>
+                      <Detay onPress={() => DetailPage(login)}>
+                        <Label>Detay =></Label>
+                      </Detay>
+                    </Box>
+                  );
+                })}
+            </ScrollView>
             <TouchableHighlight
               style={{...styles.openButton, backgroundColor: '#2196F3'}}
               onPress={() => {
@@ -47,7 +82,7 @@ const FollowModal = ({props}) => {
       </Modal>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   centeredView: {
@@ -80,6 +115,7 @@ const styles = StyleSheet.create({
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
+    fontSize: 20,
     textAlign: 'center',
   },
   modalText: {
@@ -88,4 +124,32 @@ const styles = StyleSheet.create({
   },
 });
 
+const Detay = styled.TouchableHighlight`
+  background: #0098ff;
+  margin-top: 15px;
+  width: 150px;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+`;
+
+const Box = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+  border: 1px solid #0098ff;
+  border-radius: 10px;
+`;
+const Info = styled.Text`
+  font-size: 25px;
+  color: red;
+  margin-top: 15px;
+`;
+
+const Label = styled.Text`
+  font-size: 18px;
+  color: white;
+  margin-top: 15px;
+`;
 export default FollowModal;
